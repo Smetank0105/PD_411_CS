aa6d7a494580759b582f6a8905d789fa1d0a82af
User <User@DESKTOP-QHG18FL>
2025-04-30 16:24:12 +0300

    Arrays

--- Introduction/DataTypes/Program.cs
+++ Introduction/DataTypes/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//#define DATA_TYPES
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +12,23 @@ namespace DataTypes
     {
         static void Main(string[] args)
         {
+
+#if DATA_TYPES
             Console.WriteLine("DataTypes");
             Console.WriteLine(typeof(sbyte));
             Console.WriteLine(sizeof(byte));
             Console.WriteLine($"Byte:  {byte.MinValue} ... {byte.MaxValue}");
-            Console.WriteLine($"SByte:{sbyte.MinValue}... {sbyte.MaxValue}");
+            Console.WriteLine($"SByte:{sbyte.MinValue}... {sbyte.MaxValue}"); 
+#endif
+
+            Console.WriteLine("Hello Constants");
+            Console.WriteLine('+'.GetType());
+            Console.WriteLine(5.GetType());
+            Console.WriteLine(true.GetType());
+
+            int a = -2;
+            uint b = (uint)a;
+            //C-like notation (C-подобная форма записи)
         }
     }
 }
