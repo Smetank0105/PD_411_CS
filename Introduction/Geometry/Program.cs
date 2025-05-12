465ce3ac885dc39d0879976394465a8f892708a9
User <User@DESKTOP-QHG18FL>
2025-05-12 16:14:25 +0300

    Rhombus DONE

---
+++ Introduction/Geometry/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите размер фигуры: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = i; j < n; j++) Console.Write(" ");		Console.Write("/");
+				for (int j = 0; j < i; j++) Console.Write("  ");	Console.Write("\\");
+				Console.WriteLine();
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j <= i; j++) Console.Write(" ");		Console.Write("\\");
+				for (int j = i; j < n-1; j++) Console.Write("  ");		Console.Write("/");
+				Console.WriteLine();
+			}
+		}
+	}
+}
