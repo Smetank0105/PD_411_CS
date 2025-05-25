b7c3a1917ea9598e4394d72b4c54205958352b34
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2025-05-25 14:33:23 +0700

    Square DONE

---
+++ Inheritance/Geometry/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Geometry
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IntPtr hwnd = GetConsoleWindow();
+			Graphics graphics = Graphics.FromHwnd(hwnd);
+			Rectangle window_rect = new Rectangle
+				(
+				Console.WindowLeft, Console.WindowTop,
+				Console.WindowWidth, Console.WindowHeight
+				);
+			PaintEventArgs e = new PaintEventArgs(graphics, window_rect);
+			e.Graphics.DrawRectangle(new Pen(Color.Red, 2), 400, 100, 100, 100);
+			//Shape shape = new Shape(); //Невозможно создать экземпляр абстрактного класса
+			Square square = new Square(10,Color.Red);
+			square.Info();
+		}
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr GetConsoleWindow();
+		[DllImport("kernel32.dll")]
+		public static extern IntPtr GetDC(IntPtr hwnd);
+	}
+}
